3aded7cc9a33b8a9a2e422c5e8382c0531a2efcc
Aleksi Aalto <aleksi.aalto@omenia.fi>
2016-09-09 00:54:30 +0300

    fix whitespaces
    Use only whitespaces, no tabs

--- www/js/editor.js
+++ www/js/editor.js
@@ -1,33 +1,33 @@
 var Editor = (function() {
 
     function Editor() {
-		this.editorContainer = $("#editor_container");
-		this.addButtonContainer = $("#add_button_container");
+        this.editorContainer = $("#editor_container");
+        this.addButtonContainer = $("#add_button_container");
 
         this.addPattern(16);
-		this.createAddButton();
+        this.createAddButton();
     }
 
-	Editor.prototype.addPattern = function(steps) {
-		var pattern = $('<div></div>');
-    pattern.addClass('pattern_container');
-		this.editorContainer.append(pattern);
+    Editor.prototype.addPattern = function(steps) {
+        var pattern = $('<div></div>');
+        pattern.addClass('pattern_container');
+        this.editorContainer.append(pattern);
 
-		var editorRows = $('<div></div>');
-		editorRows.append(this.createTable(steps));
-		editorRows.append(this.createTable(steps));
-		editorRows.append(this.createTable(steps));
-		pattern.append(editorRows);
-		editorRows.find($("td")).click(function() {
-			$(this).toggleClass("red");
-		});
+        var editorRows = $('<div></div>');
+        editorRows.append(this.createTable(steps));
+        editorRows.append(this.createTable(steps));
+        editorRows.append(this.createTable(steps));
+        pattern.append(editorRows);
+        editorRows.find($("td")).click(function() {
+            $(this).toggleClass("red");
+        });
 
-		var patternDisplay = this.createTable(16);
-    patternDisplay.addClass('cyan');
-		pattern.prepend(patternDisplay);
-		patternDisplay.click(function() {
-			editorRows.toggleClass("hidden");
-		});
+        var patternDisplay = this.createTable(16);
+        patternDisplay.addClass('cyan');
+        pattern.prepend(patternDisplay);
+        patternDisplay.click(function() {
+            editorRows.toggleClass("hidden");
+        });
 
     var patternName = $('<div></div>');
     patternName.addClass('mdl-textfield mdl-js-textfield');
@@ -39,27 +39,27 @@ var Editor = (function() {
     patternName.append(patternInput);
     pattern.prepend(patternName);
 
-	}
+    }
 
-	Editor.prototype.createTable = function(cells) {
+    Editor.prototype.createTable = function(cells) {
         var table = $('<table class="pattern_row"></table>');
-		var tr = $("<tr></tr>");
-		for (var i = 0; i < cells; i++) {
-			tr.append('<td></td>');
-		}
-		table.append(tr);
-		return table;
-	}
+        var tr = $("<tr></tr>");
+        for (var i = 0; i < cells; i++) {
+            tr.append('<td></td>');
+        }
+        table.append(tr);
+        return table;
+    }
 
-	Editor.prototype.createAddButton = function() {
-		_this = this;
-		var addButton = $('<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">add pattern</button>');
-		this.addButtonContainer.append(addButton);
-		addButton.click(function() {
-			_this.addPattern(16);
-		});
-	}
+    Editor.prototype.createAddButton = function() {
+        _this = this;
+        var addButton = $('<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">add pattern</button>');
+        this.addButtonContainer.append(addButton);
+        addButton.click(function() {
+            _this.addPattern(16);
+        });
+    }
 
-	return Editor;
+    return Editor;
 
 })();
